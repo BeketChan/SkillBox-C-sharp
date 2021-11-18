502b055d57ea016864af212d2505b775dea662a5
Roman Litvinov <roman260479@yandex.ru>
2021-11-18 22:28:21 +0300

    'lesson-8-done'

--- Skilbox-C-sharp/Lesson-8/Program.cs
+++ Skilbox-C-sharp/Lesson-8/Program.cs
@@ -1,15 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace Lesson_8
 {
     internal class Program
     {
+        /// <summary>
+        /// Получить текст из консоли
+        /// </summary>
+        /// <param name="tittle">Сообщение перед вводом текста</param>
+        /// <returns></returns>
+        static string GetText(string tittle)
+        {
+            Console.WriteLine(tittle);
+            string text = Console.ReadLine();
+            return text;
+        }
+
+        /// <summary>
+        /// Получить целое число из консоли.
+        /// </summary>
+        /// <param name="tittle">Сообщение перед вводом текста</param>
+        /// <returns></returns>
+        static int GetInt(string tittle)
+        {
+            Console.WriteLine(tittle);
+            int N = -1;
+            bool check = false;
+            do
+            {
+                try
+                {
+                    N = Convert.ToInt32(Console.ReadLine());
+                    check = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите корректное число !");
+                    check = false;
+                }
+            } while (!check);
+            return N;
+        }
+
+        /// <summary>
+        /// Получить дробное число из консоли.
+        /// </summary>
+        /// <param name="tittle">Сообщение перед вводом текста</param>
+        /// <returns></returns>
+        static double GetDouble(string tittle)
+        {
+            Console.WriteLine(tittle);
+            double N = -1;
+            bool check = false;
+            do
+            {
+                try
+                {
+                    N = Convert.ToInt32(Console.ReadLine());
+                    check = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите корректное число !");
+                    check = false;
+                }
+            } while (!check);
+            return N;
+        }
+
         /// <summary>
         /// Проверка корректности ввода целых чисел в указанный диапазон с возвратом прошедшего проверку значения.
         /// </summary>
@@ -28,7 +91,7 @@ namespace Lesson_8
                 }
                 catch (Exception)
                 {
-                    N = 0;
+                    N = -1;
                 }
                 if (N >= min && N <= max) check = true;
                 else Console.WriteLine("Введите корректное число !");
@@ -40,57 +103,263 @@ namespace Lesson_8
         /// Вывод опций меню в консоль.
         /// </summary>
         /// <param name="menu">Массив с пунктами меню.</param>
-        static void MenuInConsol(string[] menu)
+        static void MenuInConsol(Dictionary<int,string> menu)
         {
-            for(int i=0; i<menu.Length; i++)
+            for (int i = 0; i < menu.Count; i++)
                 Console.WriteLine($"{i} = {menu[i]}");
         }
 
-        static Worker DeserializeDepXml(string path)
+        /// <summary>
+        /// Заполнение карты нового работника.
+        /// </summary>
+        /// <returns></returns>
+        static Worker WorkerCard()
         {
-            Worker tempWorker = new Worker();
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Worker));
-            if (File.Exists(path))
-            {
-                using (StreamReader sr = new StreamReader(path))
-                {
-                    tempWorker = xmlSerializer.Deserialize(sr) as Worker;
-                }
-            }
-            return tempWorker;
+            Worker worker = new Worker();
+            worker.LastName = GetText("Фамилия:");
+            worker.FirstName = GetText("Имя:");
+            worker.Department = GetText("Подразделение:");
+            worker.Position = GetText("Должность:");
+            worker.Salary = GetDouble("Жалование:");
+            worker.Projects = GetInt("Количество проектов:");
+
+            return worker;
         }
+
+        /// <summary>
+        /// вывод списка работников в консоль.
+        /// </summary>
+        /// <param name="workers">Коллекция работников.</param>
+        /// <param name="titles">Массив заголовков.</param>
+        static void ListInConsole(List<Worker> workers, string[] titles)
+        {
+            for (int i = 0; i < titles.Length; i++)
+                Console.Write($"{titles[i],15}");
+            
+            Console.WriteLine();
+            foreach (Worker worker in workers)
+            {
+                Console.WriteLine("{0,15}{1,15}{2,15}{3,15}{4,15}{5,15}{6,15}",
+                    worker.Id,
+                    worker.Department,
+                    worker.LastName,
+                    worker.FirstName,
+                    worker.Position,
+                    worker.Salary,
+                    worker.Projects);
+            }
+        }
+
+        /// <summary>
+        /// Добавить случайных работников.
+        /// </summary>
+        static Worker AddRandomWorker()
+        {
+            string[] FirstName = { "Циклоп", "Гамбит", "Россомаха", "Зверь","Дедпул", "Саблезуб", "Магнето", "Апокалипсис","Григорий", "Константин" };
+            string[] LastName = { "Иванов", "Петров", "Сидоров", "Романов","Жуков", "Рюриков", "Колыванов", "Бекетов","Корнилов", "Смит" };
+            string[] Departments = { "Галера 1", "Галера 2", "Галера 3", "Руководство" };
+            string[] Position = { "Быдлокодер", "Джун", "Мидл", "Лид" };
+            Random rnd = new Random(Guid.NewGuid().GetHashCode()); // утянул отсюда https://stackoverflow.com/questions/39105924/how-to-randomize-seed-in-c-sharp
+
+            Worker worker = new Worker(Departments[rnd.Next(Departments.Length)],
+                                       LastName[rnd.Next(LastName.Length)],
+                                       FirstName[rnd.Next(FirstName.Length)],
+                                       Position[rnd.Next(Position.Length)],
+                                       (rnd.Next(1000, 10001) / 10.00),
+                                       rnd.Next(1, 6));
+            
+            return worker;
+        }
+
+        /// <summary>
+        /// Сериализуем данные в Xml.
+        /// </summary>
+        /// <param name="workers">Коллекция с работниками.</param>
+        /// <param name="path">Путь к файлу.</param>
+        static void SerializeXml(List<Worker> workers, string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Worker>));
+            using(StreamWriter sw = new StreamWriter(path))
+            {
+                serializer.Serialize(sw, workers);
+            }
+        }
+
+        /// <summary>
+        /// Десериализуем список работников из Xml.
+        /// </summary>
+        /// <param name="path">Путь к файлу.</param>
+        /// <returns></returns>
+        static List<Worker> DeserializeXml(string path)
+        {
+            List<Worker> tempWorkers = new List<Worker>();
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Worker>));
+            using (StreamReader sr = new StreamReader(path))
+            {
+                tempWorkers = xmlSerializer.Deserialize(sr) as List<Worker>;
+            }
+            return tempWorkers;
+        }
+
+        /// <summary>
+        /// Сериализуем данные в Json.
+        /// </summary>
+        /// <param name="workers">Коллекция с работниками.</param>
+        /// <param name="path">Путь к файлу.</param>
+        static void SerializeJson(List<Worker> workers, string path)
+        {
+            string json = JsonConvert.SerializeObject(workers, Formatting.Indented);
+            File.WriteAllText(path, json);
+        }
+
+        /// <summary>
+        /// Десериализуем список работников из Json.
+        /// </summary>
+        /// <param name="path">Путь к файлу.</param>
+        /// <returns></returns>
+        static List<Worker> DeserializeJson(string path)
+        {
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<List<Worker>>(json);
+        }
+        
+        /// <summary>
+        /// Начало работы программы.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             string pathXml = "hrBase.xml", pathJson = "hrBase.json";
-            int option = 1;
-            string[] menu = { "Закрыть программу",
-                              "Добавить сотрудника",
-                              "Удалить сотрудника",
-                              "Отобразить список сотрудников",
-                              "Упорядочить список по одному полю",
-                              "Упорядочить список по двум поям",
-                              "Отфильтровать по Отделу",
-                              "Отобразить уникальные зарплаты",
-                              "Загрузить из XML",
-                              "Сохранить в XML",
-                              "Загрузить из JSON",
-                              "Сохранить в JSON" };
-            List<Worker> depList = new List<Worker>();
-            
+            int option;
+            int numer = 0;
+
+            var menu = new Dictionary<int,string>();
+            menu.Add(numer++, "Закрыть программу");
+            menu.Add(numer++, "Добавить сотрудника");
+            menu.Add(numer++, "Удалить сотрудника");
+            menu.Add(numer++, "Отобразить список сотрудников");
+            menu.Add(numer++, "Упорядочить список по одному полю");
+            menu.Add(numer++, "Упорядочить список по двум поям");
+            menu.Add(numer++, "Отобразить уникальные имена");
+            menu.Add(numer++, "Загрузить из XML");
+            menu.Add(numer++, "Сохранить в XML");
+            menu.Add(numer++, "Загрузить из JSON");
+            menu.Add(numer++, "Сохранить в JSON");
+            menu.Add(numer++, "Сгенерировать 10 сотрудников");
+
+            string[] titles = { "ID", "Подразделение", "Фамилия", "Имя", "Должность", "Жалование", "Проекты" };
+
+            List<Worker> workers = new List<Worker>();
+
+            HashSet<string> unicName = new HashSet<string>();
+
+            List<Departments> departments = new List<Departments>();
+            departments.Add(new Departments("Галера 1"));
+            departments.Add(new Departments("Галера 2"));
+            departments.Add(new Departments("Галера 3"));
+            departments.Add(new Departments("Руководство"));
+
             do
             {
                 Console.WriteLine("Программа по работе с базой сотрудников запущена. Выберите опцию:");
                 MenuInConsol(menu);
-                option = CheckValidInput(0, menu.Length);
+                option = CheckValidInput(0, menu.Count - 1);
 
                 switch (menu[option])
                 {
                     case "Закрыть программу":
                         option = 0;
                         break;
+
+                    case "Добавить сотрудника":
+                        Worker tempWorker1 = WorkerCard();
+                        foreach (Departments D in departments)
+                            if (D.Name == tempWorker1.Department)
+                            {
+                                if (D.Count < D.WorkerLimit)
+                                {
+                                    workers.Add(WorkerCard());
+                                    D.Count++;
+                                }
+                                else Console.WriteLine($"В подразделении {D.Name} уже максимально возможное количество работников.");
+                            }                        
+                        break;
+
+                    case "Удалить сотрудника":
+                        workers.Remove(new Worker() { Id = GetInt("\nВведите уникальный номер (ID):\n") });
+                        break;
+
+                    case "Отобразить список сотрудников":
+                        ListInConsole(workers, titles);
+                        break;
+
+                    case "Упорядочить список по одному полю":
+                        Console.WriteLine("\nСписок упорядочен по Подразделению.\n");
+                        var sort1 = from Worker in workers
+                                    orderby Worker.Department
+                                    select Worker;
+                        
+                        foreach (Worker w in sort1)
+                        {
+                            workers.Remove(new Worker() { Id = w.Id });
+                            workers.Add(w);
+                        }   
+                        break;
+
+                    case "Упорядочить список по двум поям":
+                        Console.WriteLine("\nСписок упорядочен по Подразделению и Жалованию.\n");
+                        var sort2 = from Worker in workers
+                                    orderby Worker.Department, Worker.Salary
+                                    select Worker;
+
+                        foreach (Worker w in sort2)
+                        {
+                            workers.Remove(new Worker() { Id = w.Id });
+                            workers.Add(w);
+                        }
+                        break;
+
+                    case "Отобразить уникальные имена":
+                        foreach (Worker w in workers) unicName.Add(w.FirstName);
+                        Console.WriteLine("\nУникальные имена:\n");
+                        foreach (string i in unicName) Console.WriteLine(i);
+                        Console.WriteLine();
+                        break;
+
                     case "Загрузить из XML":
-                        if (!File.Exists(pathXml)) Console.WriteLine("БД XML ещё не создана !");
-                        //else 
+                        if (!File.Exists(pathXml)) Console.WriteLine("\nБД XML ещё не создана !\n");
+                        else workers = DeserializeXml(pathXml);
+                        break;
+
+                    case "Сохранить в XML":
+                        SerializeXml(workers, pathXml);
+                        break;
+
+                    case "Загрузить из JSON":
+                        if (!File.Exists(pathJson)) Console.WriteLine("\nБД XML ещё не создана !\n");
+                        else workers = DeserializeJson(pathJson);
+                        break;
+
+                    case "Сохранить в JSON":
+                        SerializeJson(workers, pathJson);
+                        break;
+
+                    case "Сгенерировать 10 сотрудников":
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Worker tempWorker2 = AddRandomWorker();
+                            foreach (Departments D in departments)
+                                if (D.Name == tempWorker2.Department)
+                                {
+                                    if (D.Count < D.WorkerLimit)
+                                    {
+                                        workers.Add(tempWorker2);
+                                        D.Count++;
+                                    }
+                                    else Console.WriteLine($"В подразделении {D.Name} уже максимально возможное количество работников.");
+                                }
+                        }
+                            
                         break;
                 }
 
--- Skilbox-C-sharp/Lesson-8/Worker.cs
+++ Skilbox-C-sharp/Lesson-8/Worker.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson_8
 {
-    public class Worker
+    public class Worker : IEquatable<Worker>
     {
         #region Свойства
+
+        /// <summary>
+        /// Уникальный номер.
+        /// </summary>
+        //[field: NonSerialized] // Метод был provat set. Но не желал сериализовываться. Пробовал исключить - не сработало. Не понял, почему.
+        public int Id { get; set; }
 
         /// <summary>
         /// Отдел.
@@ -16,14 +18,14 @@ namespace Lesson_8
         public string Department { get; set; }
 
         /// <summary>
-        /// Имя.
-        /// </summary>
-        public string FirstName { get; set; }
-
-        /// <summary>
         /// Фамилия.
         /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string FirstName { get; set; }
 
         /// <summary>
         /// Должность.
@@ -38,7 +40,7 @@ namespace Lesson_8
         /// <summary>
         /// Количество проектов.
         /// </summary>
-        public int Projects { get; private set; }
+        public int Projects { get; set; }
 
         #endregion
 
@@ -48,10 +50,38 @@ namespace Lesson_8
         /// Пустой конструктор карточки работника.
         /// </summary>
         public Worker() { }
-        
+
         /// <summary>
         /// Карточка работника.
         /// </summary>
+        /// <param name="Id">Уникальный номер</param>
+        /// <param name="Department">Подразделениею</param>
+        /// <param name="LastName">Фамилия.</param>
+        /// <param name="FirstName">Имя.</param>
+        /// <param name="Position">Должность.</param>
+        /// <param name="Salary">Зарплата.</param>
+        /// <param name="Projects">Количесвто проектов.</param>
+        public Worker(int Id,
+                      string Department,
+                      string LastName,
+                      string FirstName,
+                      string Position,
+                      double Salary,
+                      int Projects)
+        {
+            this.Id = Id;
+            this.Department = Department;
+            this.LastName = LastName;
+            this.FirstName = FirstName;
+            this.Position = Position;
+            this.Salary = Salary;
+            this.Projects = Projects;
+        }
+
+        /// <summary>
+        /// Карточка работника.
+        /// </summary>
+        /// <param name="Department">Подразделениею</param>
         /// <param name="LastName">Фамилия.</param>
         /// <param name="FirstName">Имя.</param>
         /// <param name="Position">Должность.</param>
@@ -64,12 +94,42 @@ namespace Lesson_8
                       double Salary,
                       int Projects)
         {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            Id = random.Next();
             this.Department = Department;
             this.LastName = LastName;
             this.FirstName = FirstName;
             this.Position = Position;
             this.Salary = Salary;
             this.Projects = Projects;
+        }
+
+        #endregion
+
+        #region Методы
+
+        /// <summary>
+        /// Реализацию удаления элемента коллекции посмотрел тут: https://docs.microsoft.com/ru-ru/dotnet/api/system.collections.generic.list-1.remove?view=netframework-4.8
+        /// </summary>
+        /// <param name="obj">Объект.</param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Worker worker = obj as Worker;
+            if (worker == null) return false;
+            else return Equals(worker);
+        }
+
+        /// <summary>
+        /// Сравнение текущего элемента коллекции с образцом.
+        /// </summary>
+        /// <param name="other">Образец.</param>
+        /// <returns></returns>
+        public bool Equals(Worker other)
+        {
+            if (other == null) return false;
+            return (Id.Equals(other.Id));
         }
 
         #endregion
